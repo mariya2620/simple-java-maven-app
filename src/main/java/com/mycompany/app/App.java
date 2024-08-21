f59e5742f5c9f88bfe69b98c09f1682bc9284287
mariya2620 <mariyaphilipchirayath@gmail.com>
2024-08-21 16:03:48 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println(MESSAGE);
         System.out.println(MESSAGE);
+        System.out.println(MESSAGE);
+        
     }
 
     public String getMessage() {
